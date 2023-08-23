cf603c7346e079e635b5e3a17eb764266302403d
Aleksey Efimov <smaylik121@gmail.com>
2023-08-23 09:13:20 +0300

    fix

---
+++ RickAndMorty1221/Views/EpisodesView.swift
@@ -0,0 +1,62 @@
+//
+//  EpisodesView.swift
+//  RickAndMorty1221
+//
+//  Created by Aleksey Efimov on 23.08.2023.
+//
+
+import SwiftUI
+
+struct EpisodesView: View {
+    let character: Character
+    let greenColor: Color
+    let grayColor: Color
+    
+    @State var episodes = [Episode]()
+    
+    var body: some View {
+        HStack {
+            Text("Episodes")
+                .bold()
+                .foregroundColor(.white)
+                .padding(.leading, 24.0)
+            Spacer()
+        }.onAppear() {
+            for linkEpisode in character.episode {
+                FetchEpisode().getDate(from: linkEpisode) { (results) in
+                    self.episodes.append(results)
+                }
+            }
+        }
+        ForEach(episodes, id: \.self) { episode in
+            ZStack {
+                Rectangle()
+                    .frame(height: 86)
+                    .foregroundColor(grayColor)
+                    .cornerRadius(16)
+                VStack (spacing: 10) {
+                    HStack {
+                        Text(episode.name ?? "")
+                            .bold()
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                    HStack {
+                        Text(episode.episode ?? "")
+                            .foregroundColor(greenColor)
+                        Spacer()
+                        Text(episode.air_date ?? "")
+                            .foregroundColor(.gray)
+                    }.font(.system(size: 13))
+                }
+                .padding(.horizontal, 16.0)
+            }.padding(.horizontal, 20)
+        }
+    }
+}
+
+//struct EpisodesView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        EpisodesView()
+//    }
+//}
